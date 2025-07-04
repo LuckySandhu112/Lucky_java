26c9997267626ea00d457b61c2357c1f7e7a942b
LuckySandhu112 <luckysandhu1209@gmail.com>
2025-07-04 23:11:12 +0530

    Create +ve-veZero.java

---
+++ +ve-veZero.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class PoNeZero
+{
+    public static void main(String args[])
+    {
+        int num=-1;
+        if(num>0)
+        {
+            System.out.println("It is a positive number: "+ num);
+        }
+        else if(num<0)
+        {
+            System.out.println("It is a negative number: "+ num);
+        }
+        else if(num==0)
+        {
+            System.out.println("It is zero: "+ num);
+        }
+        else
+        {
+            System.out.println("The number is incorrect: "+ num);
+        }
+    }
+}
