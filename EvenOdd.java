00b8c010838d9cae31dfa341c7bd43fb0a093a29
LuckySandhu112 <luckysandhu1209@gmail.com>
2025-07-04 23:09:42 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class EvenOdd
+{
+    public static void main(String args[])
+    {
+        int num = 3;
+
+        if(num%2==0)
+        {
+            System.out.println("It is an Even number " + num);
+        }
+        else
+        {
+            System.out.println("It is an Odd number " + num);
+        }
+    }
+}
