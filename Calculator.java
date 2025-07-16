4a3d2e196afda1dff28570b472e01d5c833871ae
LuckySandhu112 <luckysandhu1209@gmail.com>
2025-07-16 07:47:49 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -5,31 +5,31 @@ public class Calculator
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("For Addition-1, For Subtraction-2, For Multiplication-3, For Division-4, For Remainder-5 ");
-        int num1 = sc.nextInt();
+        int CalculatorOperator = sc.nextInt();
         System.out.print("Enter 1st number: ");
-        int num2 = sc.nextInt();
+        int num1 = sc.nextInt();
         System.out.print("Enter 2nd number: ");
-        int num3 = sc.nextInt();
+        int num2 = sc.nextInt();
         switch(num1)
         {
             case 1:
-            System.out.print("The Addition is: " + (num2 + num3));
+            System.out.print("The Addition is: " + (num1 + num2));
             break;
 
             case 2:
-            System.out.print("The Subtraction is: " + (num2 - num3));
+            System.out.print("The Subtraction is: " + (num1 - num2));
             break;
 
             case 3:
-            System.out.print("The Multiplication is: " + (num2 * num3));
+            System.out.print("The Multiplication is: " + (num1 * num2));
             break;
 
             case 4:
-            System.out.print("The Division is: " + (num2 / num3));
+            System.out.print("The Division is: " + (num1 / num2));
             break;
 
             case 5:
-            System.out.print("The Remainder is: " + (num2 % num3));
+            System.out.print("The Remainder is: " + (num1 % num2));
             break;
 
             default:
