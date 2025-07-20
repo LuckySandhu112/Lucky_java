1d46d5e50b5a85fbb6a2313bfbaec5a01415f20f
LuckySandhu112 <luckysandhu1209@gmail.com>
2025-07-20 09:41:42 +0530

    Update Second_Largest_Element.java

--- Second_Largest_Element.java
+++ Second_Largest_Element.java
@@ -3,25 +3,25 @@ public class Second_Largest_Element
 {
     public static void printArray(int arr[])
     {
-        for(int i = 0; i < arr.length; i++)
+        for(int num = 0; num < arr.length; num++)
         {
-            System.out.print(arr[i]+ " ");
+            System.out.print(arr[num]+ " ");
         }
         System.out.println();
     }
     public static void main(String args[])
     {
         int arr[] = {1,5,3,8,4};
-        int i, j;
-            for(i = 0;i < arr.length - 1;i++)
+        int num1, num2;
+            for(num1 = 0;num1 < arr.length - 1;num1++)
             {
-                for(j = 0; j< arr.length - i - 1; j++)
+                for(num2 = 0; num2< arr.length - num1 - 1; num2++)
                 {
-                    if(arr[j]>arr[j+1])
+                    if(arr[num2]>arr[num2+1])
                     {
-                        int temp = arr[j];
-                        arr[j] = arr[j+1];
-                        arr[j+1] = temp;
+                        int temp = arr[num2];
+                        arr[num2] = arr[num2+1];
+                        arr[num2+1] = temp;
                     }
                 }
             }
