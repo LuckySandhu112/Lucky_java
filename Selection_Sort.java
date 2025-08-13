8bc13d08fcc1c2c66ce32e3060b92a2dae757887
LuckySandhu112 <luckysandhu1209@gmail.com>
2025-08-13 20:40:41 +0530

    Update Selection_Sort.java

--- Selection_Sort.java
+++ Selection_Sort.java
@@ -2,7 +2,7 @@ public class SelectionSort
 {
     public static void main(String[] args)
     {
-        int[] arr = {5, 3, 8, 4, 2};
+        int[] arr = {4, 3, 6, 1, 2};
         for (int i = 0; i < arr.length - 1; i++)
         {
             int minIndex = i;
